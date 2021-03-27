214bcf2b8cdcc647c97879589c95fcba9c63b0cc
Sreeharika <sreeharikayelle@gmail.com>
2021-03-27 21:53:05 +0530

    signup user name added

---
+++ que2.js
@@ -0,0 +1,85 @@
+const users = [
+    {
+            _id: 'ab12ex',
+            username: 'Alex',
+            email: 'alex@alex.com',
+            password: '123123',
+            createdAt:'17/05/2019 9:00 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'fg12cy',
+            username: 'Asab',
+            email: 'asab@asab.com',
+            password: '123456',
+            createdAt:'17/05/2019 9:30 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'zwf8md',
+            username: 'Brook',
+            email: 'brook@brook.com',
+            password: '123111',
+            createdAt:'17/05/2019 9:45 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'eefamr',
+            username: 'Martha',
+            email: 'martha@martha.com',
+            password: '123222',
+            createdAt:'17/05/2019 9:50 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'ghderc',
+            username: 'Thomas',
+            email: 'thomas@thomas.com',
+            password: '123333',
+            createdAt:'17/05/2019 10:00 AM',
+            isLoggedIn: false
+    }
+    ];
+
+    const products = [
+{
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+        { userId: 'fg12cy', rate: 5 },
+        { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+},
+{
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+},
+{
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+}
+];
+// a. Create a function called ***signUp*** which allows user to add to the collection. If user exists, inform the user that he has already an account.
+function signUp(name){
+for(user of users){
+    if(user.username === name){
+        console.log('user already exist');
+    } else {
+        users.push({username : name});
+        break;
+    }
+}
+}
+signUp('Thomas');
+console.log(users);
